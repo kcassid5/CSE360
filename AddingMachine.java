9670ee37be152d11c622d6a9a2df7b211b7152c6
Kendall Cassidy <kendallcassidy@Kendalls-MacBook-Pro.local>
2020-10-05 15:31:12 -0700

    AddingMachine.java

---
+++ AddingMachine.java
@@ -0,0 +1,30 @@
+package cse360assignment02;
+//Kendall Cassidy
+//CSE 360
+//Due October 5, 2020
+
+//public class
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  //return total value
+  public int getTotal () {
+    return 0;
+  }
+  //add values
+  public void add (int value) {
+  }
+//subtract values
+  public void subtract (int value) {
+  }
+//return value with string
+  public String toString () {
+    return "";
+  }
+//clear input
+  public void clear() {
+  }
+}
