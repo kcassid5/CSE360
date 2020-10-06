1f85ed73054a7f3a1d696a76b15c2280d9655d0c
Kendall Cassidy <kendallcassidy@Kendalls-MacBook-Pro.local>
2020-10-05 20:03:31 -0700

    submit

--- AddingMachine.java
+++ AddingMachine.java
@@ -1,30 +1,43 @@
 package cse360assignment02;
+
 //Kendall Cassidy
 //CSE 360
 //Due October 5, 2020
 
-//public class
-public class AddingMachine {
-  private int total;
-  
-  public AddingMachine () {
-    total = 0;  // not needed - included for clarity
-  }
-  //return total value
-  public int getTotal () {
-    return 0;
-  }
-  //add values
-  public void add (int value) {
-  }
-//subtract values
-  public void subtract (int value) {
-  }
-//return value with string
-  public String toString () {
-    return "";
-  }
-//clear input
-  public void clear() {
-  }
+//
+
+
+public class AddingMachine{
+
+	//public class AddingMachine {
+	private int total;
+	StringBuffer history;
+
+	public AddingMachine() {
+		total = 0;  // not needed - included for clarity
+		history = new StringBuffer("0");
+
+	}
+	//return total value
+	public int getTotal () {
+		return total;
+	}
+	//add values
+	public void add (int value) {
+		total = total + value;
+		history.append(" + " + value);
+	}
+	//subtract values
+	public void subtract (int value) {
+		total = total - value;
+		history.append(" - " + value); 
+	}
+	//return value with string
+	public String toString () {
+		return history.toString();
+	}
+	//clear input
+	public void clear() {
+	}
+
 }
